643632dd2b26681bb64e9d43c25cb8c20b973bc2
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-02-25 20:27:05 -0600

    heavy changes
    structure location calcs, structure placement, etc.

---
+++ two_d_craft/src/main/java/com/quidvio/two_d_craft/LinearStructurePlacement.java
@@ -0,0 +1,26 @@
+package com.quidvio.two_d_craft;
+
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.gen.chunk.placement.StructurePlacement;
+import net.minecraft.world.gen.chunk.placement.StructurePlacementCalculator;
+import net.minecraft.world.gen.chunk.placement.StructurePlacementType;
+
+import java.util.Optional;
+
+/*
+public class LinearStructurePlacement extends StructurePlacement {
+    protected LinearStructurePlacement(Vec3i locateOffset, FrequencyReductionMethod frequencyReductionMethod, float frequency, int salt, Optional<ExclusionZone> exclusionZone) {
+        super(locateOffset, frequencyReductionMethod, frequency, salt, exclusionZone);
+    }
+
+    @Override
+    protected boolean isStartChunk(StructurePlacementCalculator calculator, int chunkX, int chunkZ) {
+        return false;
+    }
+
+    @Override
+    public StructurePlacementType<?> getType() {
+        return null;
+    }
+}
+ */
